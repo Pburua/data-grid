9ef719b90a10b00fa85e151f00b1753998b8df16
pburya <pburya@gmail.com>
2020-03-12 23:08:38 +0300

    feat: add filter controls with materialUI

--- src/js/components/FilterControls/FilterControls.tsx
+++ src/js/components/FilterControls/FilterControls.tsx
@@ -1,17 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
+import { Container, MenuItem, TextField } from '@material-ui/core';
+import './FilterControls.scss';
+
+const isActiveValues = [
+  {
+    value: 'undefined',
+    label: 'both',
+  },
+  {
+    value: 'active',
+    label: 'yes',
+  },
+  {
+    value: 'inactive',
+    label: 'no',
+  },
+];
 
 const FilterControls = (props: any) => {
   const { filterCriteria } = props;
+  const [searchText, setSearchText]: any = useState('');
+  const [isActive, setIsActive]: any = useState('undefined');
 
   console.log(filterCriteria);
 
+  function log(text) {
+    console.log(text);
+  }
+
+  function handleSearchTextChange(event) {
+    setSearchText(event.target.value);
+  }
+
+  function handleIsActiveChange(event) {
+    setIsActive(event.target.value);
+    log(isActive);
+  }
+
   return (
-    <>
-      <input type="text" />
-      <input type="checkbox" />
-      {/*  TODO: add material ui here before proceeding */}
-    </>
+    <Container className="filter-controls">
+      <TextField
+        className="filter-controls__item"
+        id="search-text"
+        label="Filter text"
+        value={searchText}
+        onBlur={() => {
+          log(searchText);
+        }}
+        onChange={handleSearchTextChange}
+      />
+      <TextField
+        className="filter-controls__item"
+        id="is-active"
+        select
+        label="Active"
+        value={isActive}
+        onChange={handleIsActiveChange}
+      >
+        {isActiveValues.map((option) => (
+          <MenuItem key={option.value} value={option.value}>
+            {option.label}
+          </MenuItem>
+        ))}
+      </TextField>
+    </Container>
   );
 };
 
