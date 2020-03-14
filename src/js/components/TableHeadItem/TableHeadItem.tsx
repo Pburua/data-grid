bcab6f3bcadf85a61ac71562bbd95f072eb647f6
pburya <pburya@gmail.com>
2020-03-14 12:22:28 +0300

    feat: add toggling arrows

--- src/js/components/TableHeadItem/TableHeadItem.tsx
+++ src/js/components/TableHeadItem/TableHeadItem.tsx
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TableCell } from '@material-ui/core';
 import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 import './TableHeadItem.scss';
 
 const TableHeadItem = ({ type, text }: any) => {
+  const [sortDown, setSortDown] = useState(true);
+
+  function handleTableHeadCellCLick() {
+    setSortDown(!sortDown);
+  }
 
   return (
-    <TableCell className={`table__cell table-head__cell ${type}`} component="div">
+    <TableCell className={`table__cell table-head__cell ${type}`} onClick={handleTableHeadCellCLick} component="div">
       <div className="table-head__text">
         {text}
       </div>
-      <div className="table-head__sort-controls sort-controls">
-        <ArrowDropUpIcon className="sort-controls__up" fontSize="small" />
-        <ArrowDropDownIcon className="sort-controls__up" fontSize="small" />
+      <div className="table-head__sort-controls">
+        {sortDown ? <ArrowDropUpIcon fontSize="small" /> : <ArrowDropDownIcon fontSize="small" />}
       </div>
     </TableCell>
   );
